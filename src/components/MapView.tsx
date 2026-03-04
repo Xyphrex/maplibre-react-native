acf27bf07d085b1e47d9b906e84312113d0d2e0e
Justin/usr/local/bin/python3 <jhoogwaerts@gmail.com>
2026-03-05 00:42:14 +1100

    feat: added onTouchStart implementation for android

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -160,6 +160,10 @@ interface MapViewProps extends BaseProps {
    */
   onPress?: (feature: GeoJSON.Feature) => void;
   /**
+   * Map touch start listener, gets called when a user starts touching the map
+   */
+  onTouchStart?: (feature: GeoJSON.Feature) => void;
+  /**
    * Map long press listener, gets called when a user long presses the map
    */
   onLongPress?: (feature: GeoJSON.Feature) => void;
@@ -238,9 +242,10 @@ type CallableProps = {
     : never;
 }[keyof MapViewProps];
 
-interface NativeProps extends Omit<MapViewProps, "onPress" | "onLongPress"> {
+interface NativeProps extends Omit<MapViewProps, "onPress" | "onTouchStart" | "onLongPress"> {
   mapStyle?: string;
   onPress(event: NativeSyntheticEvent<{ payload: GeoJSON.Feature }>): void;
+  onTouchStart(event: NativeSyntheticEvent<{ payload: GeoJSON.Feature }>): void;
   onLongPress(event: NativeSyntheticEvent<{ payload: GeoJSON.Feature }>): void;
 }
 
@@ -610,6 +615,14 @@ export const MapView = memo(
         }
       };
 
+      const _onTouchStart = (
+        e: NativeSyntheticEvent<{ payload: GeoJSON.Feature }>,
+      ): void => {
+        if (isFunction(props.onTouchStart)) {
+          props.onTouchStart(e.nativeEvent.payload);
+        }
+      };
+
       const _onLongPress = (
         e: NativeSyntheticEvent<{ payload: GeoJSON.Feature }>,
       ): void => {
@@ -802,6 +815,7 @@ export const MapView = memo(
       const callbacks = {
         ref: (ref: MLRNMapViewRefType): void => _setNativeRef(ref),
         onPress: _onPress,
+        onTouchStart: _onTouchStart,
         onLongPress: _onLongPress,
         onMapChange: _onChange,
         onAndroidCallback: isAndroid() ? _onAndroidCallback : undefined,
